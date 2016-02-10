c7e7d43cc3d418bad592313f23facdc004763daa
lucia <lucieclau.97@outlook.com>
2016-02-10 10:50:51 -0500

    actual

--- CLASE 01/DemoWeb01/src/java/pe/egcc/servlets/MateServlet.java
+++ CLASE 01/DemoWeb01/src/java/pe/egcc/servlets/MateServlet.java
@@ -25,7 +25,7 @@ public class MateServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hora del break!!!</h1>");
+        out.println("<h1>Hora del breakjnijninij!!!</h1>");
     }
     
 
